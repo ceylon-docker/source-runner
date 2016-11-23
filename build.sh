82161cd990ee103fde5895cc3480d31dd06450df
Tako Schotanus <tako@ceylon-lang.org>
2016-11-23 14:48:03 +0100

    Made push much more efficient

--- build.sh
+++ build.sh
@@ -101,10 +101,8 @@ function build_dir() {
         echo "Building image..."
         docker build -t "${IMAGE}:$NAME" $QUIET .
     fi
-    [[ $PUSH -eq 1 ]] && echo "Pushing image to Docker Hub..." && docker push "${IMAGE}:$NAME"
     for t in ${TAGS[@]}; do
         [[ $BUILD -eq 1 ]] && docker tag "${IMAGE}:$NAME" "${IMAGE}:$t"
-        [[ $PUSH -eq 1 ]] && docker push "${IMAGE}:$t"
     done
     if [[ $CLEAN -eq 1 ]]; then
         echo "Removing image..."
@@ -177,4 +175,6 @@ function build() {
 for v in ${VERSIONS[@]}; do
     build $v
 done
+
+[[ $PUSH -eq 1 ]] && echo "Pushing image to Docker Hub..." && docker push "${IMAGE}"
 
